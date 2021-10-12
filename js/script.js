8d5195481552bcddd7a49b683cf0cfa00128c6c2
Marcin Dzikowicz <mistamasta7@gmail.com>
2021-10-12 22:12:23 +0200

    Edit js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,31 @@
 'use strict';
-const titleClickHandler = function(event){
-  console.log('Link was clicked!');
+const titleClickHandler = function(event) {
+    console.log('Link was clicked!');
 
-  /* remove class 'active' from all article links  */
+    /* remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
-  /* add class 'active' to the clicked link */
+    for (let activeLink of activeLinks) {
+        activeLink.classList.remove('active');
+    }
 
-  /* remove class 'active' from all articles */
+    /* add class 'active' to the clicked link */
 
-  /* get 'href' attribute from the clicked link */
+    /* remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.posts article.active');
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+    for (let activeLink of activeLinks) {
+        activeArticle.classList.remove('active');
 
-  /* add class 'active' to the correct article */
+    /* get 'href' attribute from the clicked link */
+
+    /* find the correct article using the selector (value of 'href' attribute) */
+
+    /* add class 'active' to the correct article */
 }
 
 const links = document.querySelectorAll('.titles a');
 
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
+for (let link of links) {
+    link.addEventListener('click', titleClickHandler);
 }

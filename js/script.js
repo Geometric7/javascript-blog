e5ab6a600e23fd0286cc7d098c3ce3f400fe2879
Marcin Dzikowicz <mistamasta7@gmail.com>
2021-10-12 22:19:39 +0200

    Add js

--- js/script.js
+++ js/script.js
@@ -2,16 +2,18 @@
 const titleClickHandler = function(event) {
     console.log('Link was clicked!');
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for (let activeLink of activeLinks) {
         activeLink.classList.remove('active');
     }
 
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
+    console.log('clickedElement:', clickedElement);
 
-    /* remove class 'active' from all articles */
+    /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
 
     for (let activeLink of activeLinks) {
